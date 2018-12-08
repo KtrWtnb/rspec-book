ba52b8f0933c68bc68b0c2a786057b7e78f62ddd
KtrWtnb <whatnowbay.k3nx2@gmail.com>
2018-12-08 16:34:25 +0900

    DRY by helper methods when failure

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -1,15 +1,18 @@
 describe 'An ideal sandwich' do
-  before { @sandwich = Sandwich.new('delicious', []) }
   Sandwich = Struct.new(:taste, :toppings)
+
+  def sandwich
+    Sandwich.new('delicious', [])
+  end
   it 'is delicious' do
-    taste = @sandwich.taste
+    taste = sandwich.taste
 
     expect(taste).to eq('delicious')
   end
 
   it 'lets me add toppings' do
-    @sandwich.toppings << 'cheese'
-    toppings = @sandwich.toppings
+    sandwich.toppings << 'cheese'
+    toppings = sandwich.toppings
 
     expect(toppings).not_to be_empty
   end
