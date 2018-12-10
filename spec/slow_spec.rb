87942d5628fb06aa5d2907bcda08386e1557ca76
KtrWtnb <whatnowbay.k3nx2@gmail.com>
2018-12-10 15:52:35 +0900

    add sleep_spec

---
+++ spec/slow_spec.rb
@@ -0,0 +1,7 @@
+describe 'The sleep() method' do
+  it('should can sleep for 0.1 second') { sleep 0.1 }
+  it('should can sleep for 0.1 second') { sleep 0.2 }
+  it('should can sleep for 0.1 second') { sleep 0.3 }
+  it('should can sleep for 0.1 second') { sleep 0.4 }
+  it('should can sleep for 0.1 second') { sleep 0.5 }
+end
